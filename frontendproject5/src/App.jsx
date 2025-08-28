e9919e829764cfe34199c61571d843c76ce03ce4
rajucreate <svaradaraju8@gmail.com>
2025-08-28 10:27:31 +0530

    Updated Javascript and frontendproject5 styles

--- frontendproject5/src/App.jsx
+++ frontendproject5/src/App.jsx
@@ -1,12 +1,48 @@
 import React, { Component } from 'react'
 
 export default class App extends Component {
+
+  constructor(p){
+    super(p);
+    this.state={
+      expression: "",
+      result: ""
+    };
+  }
+
+  calculate = () =>{
+    try{
+      const result = ecal(this.state.expression);
+      this.setState({ result });
+    }
+    catch(err){
+      this.setState({result:"Enter valid expression"});
+    }
+  }
+
+
   render() {
     return (
       <>
         <header>
           <h1>Simple Calculator</h1>
         </header>
+        <section>
+          <div className="calculator">
+            <input 
+              type="text" 
+              placeholder="Enter expression" 
+              value={this.state.expression}
+              onChange={this.handleChange}
+            />
+            <button onClick="this.calculate">Calculate</button>
+            <div className="result">Result: {this.state.result} </div>
+          </div>
+        </section>
+        
+        <footer>
+          <h1>Have Fun !!!</h1>
+        </footer>
       </>
     )
   }
